8676afe4b293aefd930115b8ba84778ce2d95e10
Subhasree370 <subhasreemaji190@gmail.com>
2022-12-07 02:06:23 +0530

    2nd msg

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 const adminRoutes = require("./routes/admin");
 const blogRoutes = require("./routes/blog");
+
+app.set('view engine', 'ejs'); // set template extension for express(ejs, pug, handleBar)
+app.set('views', 'views'); // set template folder as views
+
 
 //adminRoutes middleware//
 app.use('/admin', adminRoutes);
@@ -14,7 +19,8 @@ app.use(blogRoutes);
 
 //404 page not found middleware//
 app.use((req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.sendFile(path.join(__dirname, 'views', '404.html'));  
+    res.render('404', {pageTitle:"404 not found", content: "404 page not found"});
 });
 
 app.listen(3000);
--- routes/blog.js
+++ routes/blog.js
@@ -2,13 +2,34 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+const DUMMYDATA = [
+    {
+        title: "title1",
+        content: 'content1'
+    },
+    {
+        title: "title2",
+        content: 'content2'
+    },
+    {
+        title: "title3",
+        content: 'content3'
+    },
+    {
+        title: "title4",
+        content: 'content4'
+    }
+];
+
 router.get('/',(req, res) =>{
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 router.get('/view-blog',(req, res) =>{
-    res.send('<h1>All blog</h1>');
+    // res.send('<h1>All blog</h1>');
+    res.render('allblog', {pageTitle:"All BLogs", data: DUMMYDATA});
 });
+
 
 
 
